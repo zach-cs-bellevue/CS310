f72c57aadeebccc53c7155ea1b7b60c90e2e7920
zach-cs-bellevue <zaseaton@my365.bellevue.edu>
2025-03-27 14:21:02 -0400

    Add files via upload

---
+++ M3 Discussion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string dayOfWeek;
+
+    // Input
+    cout << "Enter the day of the week (i.e Monday, Tuesday, etc.): ";
+    cin >> dayOfWeek;
+
+    // Selection control
+    if (dayOfWeek = "Sunday") {
+        cout << "Weekly assignments are due today, better get to it!" << endl;
+    }
+    else {
+        cout << "You still have time to complete your weekly assignments!" << endl;
+    }
+
+    return O;
+}
