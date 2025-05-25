2da756fb41c5d572d79f172ef46820ee396644d0
zach-cs-bellevue <zaseaton@my365.bellevue.edu>
2025-05-25 11:21:40 -0400

    M11 Discussion

---
+++ M11 Discussion.cpp
@@ -0,0 +1,82 @@
+/*
+M11 Discussion: Overloading and Templates
+CS310-T301: Programming with C++
+Bellevue University
+15 May, 2025
+Dr. Robert Flowers
+
+References:
+Malik, D. S. (2018). C++ Programming: From Problem Analysis to Program Design (8th ed.). Cengage Learning.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Base class
+class Character {
+protected:
+    string name;
+    int level;
+    string class_name;
+
+public:
+    Character(string n, int l, string c) : name(n), level(l), {}
+
+    // Templated function to display info
+    template <typename characterInfo>
+    void displayInfo(characterInfo primary_ability) {
+        cout << "Name: " << name << endl;
+        cout << "Level: " << level << endl;
+        cout << "Class: " << class_name << endl;
+        cout << "Primary Ability: " << primary_ability << endl;
+    }
+
+    virtual ~Character() {}
+};
+
+// Child class1
+class Sorcerer : public Character {
+public:
+    Sorcerer(string n, int l) : Character(n, l, "Sorcerer")
+
+    void displayInfo() {
+        Character::displayInfo("Charisma");
+    }
+};
+
+// Child class2
+class Druid : public Character {
+public:
+    Druid(string n, int l) : Character(n, l, "Druid") {}
+
+    void displayInfo() {
+        Character::displayInfo("Wisdom");
+    }
+};
+
+// Child class3
+class Rogue : public Character {
+public:
+    Rogue(string n, int l) : Character(n, l, "Rogue") {}
+
+    void displayInfo() {
+        Character::displayInfo("Dexterity");
+    }
+};
+
+int main() {
+    Sorcerer sorcerer("Zoey", 11);
+    Druid druid("Aine", 6);
+    Rogue rogue("Faye", 4);
+
+    cout << "\nCharacter Info:" << endl;
+    sorcerer.displayInfo();
+    cout << endl;
+    druid.displayInfo();
+    cout << endl;
+    rogue.displayInfo();
+    cout << endl;
+
+    return 0;
+}
